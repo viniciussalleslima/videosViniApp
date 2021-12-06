1e5998185ffa815197ffa535af2f2fc2be75a235
Etec 244 <61015163+geovanamoreira@users.noreply.github.com>
2021-12-06 12:28:16 -0200

    aula 117/18

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,40 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
+
+
+
+  async exibirAlertaFavorito() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      message: 'deseja realmente favoritar o filme',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+         
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Sim, favoritar!',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async apresentartoast() {
+    const toast = await this.toastController.create({
+      message: 'o filme foi adicionado aos favoritos!',
+      duration: 2000
+    });
+    toast.present();
+  }
 
 }
