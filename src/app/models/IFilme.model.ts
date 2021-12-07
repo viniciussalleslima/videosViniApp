1a84d2631a9759a957e410a9ac32fc0a31efd1b4
Etec 244 <e244ti@outlook.com>
2021-12-07 09:12:47 -0200

    projeto finalizado(to feliz)

---
+++ src/app/models/IFilme.model.ts
@@ -0,0 +1,8 @@
+export interface IFilme {
+  nome: string;
+  lancamentos: string;
+  duracao: string;
+  classificacao: number;
+  cartaz: string;
+  generos: string[];
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { IFilme } from '../models/IFilme.model';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +9,27 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Videos App';
+
+  listaVideos: IFilme[]= [
+    {
+    nome: 'Venom: Tempo de Carnificina (2021)',
+    lancamentos: '07/10/2021',
+    duracao: '1h 37m ',
+classificacao:72,
+cartaz:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/h5UzYZquMwO9FVn15R2eK2itmHu.jpg',
+generos: ['Ficção científica', 'Ação', 'Aventura']
+    },
+    {
+      nome:'Alerta Vermelho (2021)',
+      lancamentos:'05/11/2021',
+      duracao: '1h 48m',
+      classificacao: 72,
+      cartaz:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pe17f8VDfzbvbHSAKAlcORtBHmW.jpg',
+      generos:['Ação', 'Comédia', 'Crime', 'Thriller']
+    }
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
@@ -21,14 +43,13 @@ export class Tab1Page {
         {
           text: 'Cancelar',
           role: 'cancel',
-         
           handler: (blah) => {
             console.log('Confirm Cancel: blah');
           }
         }, {
           text: 'Sim, favoritar!',
           handler: () => {
-            console.log('Confirm Okay');
+            this.apresentartoast();
           }
         }
       ]
@@ -40,7 +61,8 @@ export class Tab1Page {
   async apresentartoast() {
     const toast = await this.toastController.create({
       message: 'o filme foi adicionado aos favoritos!',
-      duration: 2000
+      duration: 2000,
+      color: 'primary'
     });
     toast.present();
   }
